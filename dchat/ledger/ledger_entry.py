f85fe41d40c1e0e2ccb35e04cfcee949afb94cb6
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2022-01-06 00:37:43 -0500

    Add timestamp and unique daddr to ledger entries, TODO: check with multithreading

--- dchat/ledger/ledger_entry.py
+++ dchat/ledger/ledger_entry.py
@@ -1,9 +1,27 @@
+import time
+
+from ..utils import constants
 
 
 class LedgerEntry:
+    _current_hex_str = "0" * constants.DADDR_LENGTH
+
     def __init__(self, ip_address: str, nick_name: str) -> None:
         self.__ip_address = ip_address
         self.__nick_name = nick_name
+        self.__timestamp = time.time()
+        self.__daddr = self.__generate_next_hex()
+
+    @classmethod
+    def __generate_next_hex(cls) -> str:
+        try:
+            hex_num = int(cls._current_hex_str, 16)
+            hex_num += 1
+            prev_hex_str = cls._current_hex_str
+            cls._current_hex_str = str(hex(hex_num))[2:].upper().zfill(constants.DADDR_LENGTH)
+            return prev_hex_str
+        except ValueError as _:
+            return "-1"
 
     @property
     def ip_address(self) -> str:
@@ -14,7 +32,7 @@ class LedgerEntry:
         return self.__nick_name
 
     def __str__(self) -> str:
-        return f"{self.__ip_address}: {self.__nick_name}"
+        return f"{self.__ip_address}: {self.__nick_name} ({self.__timestamp}) -> {self.__daddr}"
 
     def __eq__(self, other: 'LedgerEntry') -> bool:
         return self.__ip_address == other.ip_address
